3b99ba501d7fa0207de1e0cb2cbe983986cdec1b
dikshan gurung <dxngrg2058@gmail.com>
2023-11-19 21:56:42 +0545

    currency_counter completed

--- currency_converter/src/App.js
+++ currency_converter/src/App.js
@@ -1,19 +1,53 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useEffect } from "react";
+import { Children, useEffect } from "react";
 import { useState } from "react";
 export default function App() {
+	const [input, setInput] = useState(0);
+	const [convertedAmount, setConvertedAmount] = useState(0);
+	const [transferFromCurr, setTransferFromCurr] = useState("EUR");
+	const [transferToCurr, setTransferToCurr] = useState("USD");
+	function handleTransferFrom(event) {
+		setTransferFromCurr(event.target.value);
+	}
+	function handleTransferTo(event) {
+		setTransferToCurr(event.target.value);
+	}
+	useEffect(
+		function () {
+			async function getCurr() {
+				// const response = await fetch(
+				// 	`https://api.frankfurter.app/latest?amount=100&from=INR&to=USD`
+				// );
+				const response = await fetch(
+					`https://api.frankfurter.app/latest?amount=${input}&from=${transferFromCurr}&to=${transferToCurr}`
+				);
+				const data = await response.json();
+				console.log(data);
+				setConvertedAmount(data.rates[transferToCurr]);
+			}
+			getCurr();
+		},
+		[input, transferFromCurr, transferToCurr]
+	);
 	return (
 		<div>
-			<Input />
-			<TransferFrom />
-			<TransferTo />
-			<Output />
+			<Input input={input} setInput={setInput} />
+			<TransferFrom
+				handleTransferFrom={handleTransferFrom}
+				value={transferFromCurr}
+			/>
+			<TransferTo
+				handleTransferTo={handleTransferTo}
+				value={transferToCurr}
+			/>
+			<Output>
+				{convertedAmount} {transferToCurr}
+			</Output>
 		</div>
 	);
 }
-function Input() {
-	const [input, setInput] = useState(0);
+function Input({ input, setInput }) {
 	function handleInputChange(event) {
 		if (isNaN(event.target.value)) {
 			setInput("NaN");
@@ -21,12 +55,11 @@ function Input() {
 		}
 		setInput(event.target.value);
 	}
-	console.log(input);
 	return <input type="text" onChange={handleInputChange} value={input} />;
 }
-function TransferFrom() {
+function TransferFrom({ handleTransferFrom, value }) {
 	return (
-		<select>
+		<select onChange={handleTransferFrom} value={value}>
 			<option value="USD">USD</option>
 			<option value="EUR">EUR</option>
 			<option value="CAD">CAD</option>
@@ -34,9 +67,9 @@ function TransferFrom() {
 		</select>
 	);
 }
-function TransferTo() {
+function TransferTo({ handleTransferTo, value }) {
 	return (
-		<select>
+		<select onChange={handleTransferTo} value={value}>
 			<option value="USD">USD</option>
 			<option value="EUR">EUR</option>
 			<option value="CAD">CAD</option>
@@ -44,10 +77,6 @@ function TransferTo() {
 		</select>
 	);
 }
-function Output() {
-	return (
-		<p>
-			OUTPUT: <span>0</span>
-		</p>
-	);
+function Output({ children }) {
+	return <p>{children}</p>;
 }
