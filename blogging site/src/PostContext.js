5a6c27b61f8f80a9738e1807e49d42ddaa7badbf
dikshan gurung <dxngrg2058@gmail.com>
2024-06-22 15:31:55 +0545

    workout timer added

--- blogging site/src/PostContext.js
+++ blogging site/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -32,18 +32,17 @@ function PostProvider({ children }) {
 	function handleClearPosts() {
 		setPosts([]);
 	}
+	const value = useMemo(() => {
+		return {
+			posts: searchedPosts,
+			onClearPosts: handleClearPosts,
+			searchQuery,
+			setSearchQuery,
+			onAddPost: handleAddPost,
+		};
+	}, [searchedPosts, searchQuery]);
 	return (
-		<PostContext.Provider
-			value={{
-				posts: searchedPosts,
-				onClearPosts: handleClearPosts,
-				searchQuery,
-				setSearchQuery,
-				onAddPost: handleAddPost,
-			}}
-		>
-			{children}
-		</PostContext.Provider>
+		<PostContext.Provider value={value}>{children}</PostContext.Provider>
 	);
 }
 
