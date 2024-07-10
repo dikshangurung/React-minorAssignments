a23a9b20b6c0835120d8e1de315fe42566ac64f2
dikshan gurung <dxngrg2058@gmail.com>
2024-07-10 22:40:31 +0545

    redux dev tools

--- redux-example/src/features/accounts/BalanceDisplay.js
+++ redux-example/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
-  }).format(value);
+	return new Intl.NumberFormat("en", {
+		style: "currency",
+		currency: "USD",
+	}).format(value);
 }
 
 function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+	const {
+		loan: currentLoan,
+		loanPurpose: currentLoanPurpose,
+		balance,
+	} = useSelector((state) => state.account);
+	return <div className="balance">{formatCurrency(123456)}</div>;
 }
 
 export default BalanceDisplay;
