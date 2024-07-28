68a1d120a5416b23790758d83884d77bef9b65d8
dikshan gurung <dxngrg2058@gmail.com>
2024-07-28 22:34:52 +0545

    tailwindcss halted for food-cart

--- food-cart-app/src/features/user/CreateUser.jsx
+++ food-cart-app/src/features/user/CreateUser.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Button from '../../ui/Button';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
@@ -16,11 +17,12 @@ function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="input m-8 w-72"
       />
 
       {username !== '' && (
         <div>
-          <button>Start ordering</button>
+          <Button>Start ordering</Button>
         </div>
       )}
     </form>
--- food-cart-app/src/ui/Error.jsx
+++ food-cart-app/src/ui/Error.jsx
@@ -1,15 +1,17 @@
-import { useNavigate } from "react-router-dom";
-import { useRouteError } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
+import LinkButton from './LinkButton';
 function NotFound() {
-	const navigate = useNavigate();
-	const error = useRouteError();
-	return (
-		<div>
-			<h1>Something went wrong 😢</h1>
-			<p>{error.data || error.message}</p>
-			<button onClick={() => navigate(-1)}>&larr; Go back</button>
-		</div>
-	);
+  const navigate = useNavigate();
+  const error = useRouteError();
+  return (
+    <div>
+      <h1>Something went wrong 😢</h1>
+      <p>{error.data || error.message}</p>
+      <LinkButton to="-1">&larr; Go back</LinkButton>
+      {/* <button onClick={() => navigate(-1)}>&larr; Go back</button> */}
+    </div>
+  );
 }
 
 export default NotFound;
