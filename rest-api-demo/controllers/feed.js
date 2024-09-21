6e0ab1e5bdc80f059c89c7faf38c94329347dd45
dikshan gurung <dxngrg2058@gmail.com>
2024-09-21 23:13:37 +0545

    express validator added

---
+++ rest-api-demo/controllers/feed.js
@@ -0,0 +1,42 @@
+const { validationResult } = require("express-validator");
+
+exports.getPosts = (req, res, next) => {
+	res.status(200).json({
+		posts: [
+			{
+				_id: "1",
+				title: "First Post",
+				content: "This is the first post!",
+				imageUrl: "images/duck.jpeg",
+				creator: {
+					name: "Maximilian",
+				},
+				createdAt: new Date(),
+			},
+		],
+	});
+};
+
+exports.createPost = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			message: "This has caused some error",
+			errors: errors.array(),
+		});
+	}
+	const title = req.body.title;
+	const content = req.body.content;
+
+	// Create post in db
+	res.status(201).json({
+		message: "Post created successfully!",
+		post: {
+			_id: new Date().toISOString(),
+			title: title,
+			content: content,
+			creator: { name: "Maximilian" },
+			createdAt: new Date(),
+		},
+	});
+};
